30dd13297401231b2f8db24fcebb82e19bca0ae4
Rupak Roy <rupak2007bob1@yahoo.co.in>
2020-02-25 19:53:41 +0530

    update

---
+++ Android/js/mal.js
@@ -0,0 +1,31 @@
+
+var map = null;
+    var markerArray = [];
+    function submit(){
+
+    var val = $("#id_one").val();
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', "http://127.0.0.1:80", true);
+    data = new FormData();
+    data.append("type", val);
+    if(val != "cancer")
+      {console.log("Here");
+       console.log(data);
+    data.append("img", $("#file_").prop('files')[0]);}
+    xhr.onreadystatechange = function(){
+
+      var obj = JSON.parse(this.responseText)[0];
+      if(!obj["empty"])
+      {
+
+        $("#status").html("<b>"+obj["para"]+"</b>")
+        $("#status1").html("<b>"+obj["unin"]+"</b>")
+        document.getElementById("demo").innerHTML = "Analysis Report";
+        clear();
+        $("#result").html("<b>"+obj["pred_val"]+"</b>")
+
+      }
+    };
+    xhr.send(data);
+  }
+ 
