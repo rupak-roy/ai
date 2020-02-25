da7060ce40b266e0e37522433421d5ebec07ccd3
Rupak Roy <rupak2007bob1@yahoo.co.in>
2020-02-25 20:02:06 +0530

    update

---
+++ WebApp/static/js/cancer.js
@@ -0,0 +1,29 @@
+
+var map = null;
+    var markerArray = [];
+    function submit(){
+
+    var val = $("#id_one").val();
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', "http://127.0.0.1:80", true);
+    var data = new FormData();
+    data.append("type", val);
+    if(val != "cancer")
+      {console.log("Here");
+    data.append("img", $("#file_").prop('files')[0]);}
+    xhr.onreadystatechange = function(){
+
+      var obj = JSON.parse(this.responseText)[0];
+      if(!obj["empty"])
+      {
+
+        $("#status").html("<b>"+obj["can"]+"</b>")
+        $("#status1").html("<b>"+obj["norm"]+"</b>")
+        document.getElementById("demo").innerHTML = "Analysis Report";
+        clear();
+        $("#result").html("<b>"+obj["pred_val"]+"</b>")
+
+      }
+    };
+    xhr.send(data);
+  }
---
+++ WebApp/static/js/pnmBK1.js
@@ -0,0 +1,32 @@
+
+var map = null;
+    var markerArray = [];
+    function submit(){
+
+    var val = $("#id_one").val();
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', "http://127.0.0.1:80", true);
+    var data = new FormData();
+    data.append("type", val);
+    if(val != "cancer")
+      {console.log("Here");
+    data.append("img", $("#file_").prop('files')[0]);}
+    xhr.onreadystatechange = function(){
+
+      var obj = JSON.parse(this.responseText)[0];
+      if(!obj["empty"])
+      {
+        
+        $("#status").html("<b>"+obj["bac"]+"</b>")
+        $("#status1").html("<b>"+obj["normal"]+"</b>")
+        $("#status2").html("<b>"+obj["viral"]+"</b>")
+
+        document.getElementById("demo").innerHTML = "Analysis Report";
+        clear();
+        $("#result").html("<b>"+obj["pred_val"]+"</b>")
+
+      }
+    };
+    xhr.send(data);
+  }
+  
